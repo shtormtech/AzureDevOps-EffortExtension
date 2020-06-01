1ea39b38f93452de122e0b99656d4c8bd6a776ba
Иван Варнавский <ivan.varnavskiy@shtormtech.ru>
2020-06-01 23:21:38 +0300

    change migration

---
+++ BackEnd/Effort.DB.Layer/Migrations/20200601201641_InitialCreate.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Effort.DB.Layer.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "effort");
+
+            migrationBuilder.CreateTable(
+                name: "activity_type",
+                schema: "effort",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Code = table.Column<string>(maxLength: 100, nullable: false),
+                    Comment = table.Column<string>(maxLength: 250, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Color = table.Column<string>(maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activity_type", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "timesheet",
+                schema: "effort",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    ActivityTypeId = table.Column<int>(nullable: false),
+                    UserUniqueName = table.Column<string>(maxLength: 250, nullable: false),
+                    WorkItemId = table.Column<int>(nullable: false),
+                    Duration = table.Column<int>(nullable: false),
+                    Comment = table.Column<string>(maxLength: 250, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Inserted = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2020, 6, 1, 23, 16, 40, 570, DateTimeKind.Local).AddTicks(1077)),
+                    LastUpdated = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2020, 6, 1, 23, 16, 40, 561, DateTimeKind.Local).AddTicks(4248))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_timesheet", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "activity_type",
+                columns: new[] { "Id", "Code", "Color", "Comment", "IsDeleted", "Name" },
+                values: new object[,]
+                {
+                    { 1, "analyze", "red", "Анализ задачи", false, "Анализ" },
+                    { 2, "develop", "green", "Разработка", false, "Разработка" },
+                    { 3, "test", "blue", "Тестирование", false, "Тестирование" }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -1L, 1, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(3280), 15, true, "iloer", 11 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -2L, 1, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5157), 15, false, "iloer", 22 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -3L, 1, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5172), 15, false, "iloer", 33 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -4L, 2, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5174), 15, true, "iloer", 44 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -5L, 2, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5178), 15, false, "iloer", 55 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -6L, 2, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5179), 15, false, "iloer", 66 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -7L, 2, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5180), 15, false, "iloer", 77 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -8L, 3, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5181), 15, true, "iloer", 88 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -9L, 3, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5182), 15, false, "iloer", 99 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -10L, 3, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5183), 15, false, "iloer", 100 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -11L, 3, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5184), 15, false, "iloer", 110 });
+
+            migrationBuilder.InsertData(
+                schema: "effort",
+                table: "timesheet",
+                columns: new[] { "Id", "ActivityTypeId", "Comment", "Date", "Duration", "IsDeleted", "UserUniqueName", "WorkItemId" },
+                values: new object[] { -12L, 3, "текст", new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5185), 15, false, "iloer", 120 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "activity_type",
+                schema: "effort");
+
+            migrationBuilder.DropTable(
+                name: "timesheet",
+                schema: "effort");
+        }
+    }
+}
--- BackEnd/Effort.DB.Layer/Migrations/EffortDbContextModelSnapshot.cs
+++ BackEnd/Effort.DB.Layer/Migrations/EffortDbContextModelSnapshot.cs
@@ -22,7 +22,7 @@ namespace Effort.DB.Layer.Migrations
 
             modelBuilder.Entity("Effort.Models.ActivityType", b =>
                 {
-                    b.Property<long>("Id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
                     b.Property<string>("Code")
@@ -36,7 +36,7 @@ namespace Effort.DB.Layer.Migrations
                     b.Property<string>("Comment")
                         .HasMaxLength(250);
 
-                    b.Property<bool>("Deleted");
+                    b.Property<bool>("IsDeleted");
 
                     b.Property<string>("Name")
                         .IsRequired()
@@ -49,29 +49,29 @@ namespace Effort.DB.Layer.Migrations
                     b.HasData(
                         new
                         {
-                            Id = 1L,
+                            Id = 1,
                             Code = "analyze",
                             Color = "red",
                             Comment = "Анализ задачи",
-                            Deleted = false,
+                            IsDeleted = false,
                             Name = "Анализ"
                         },
                         new
                         {
-                            Id = 2L,
+                            Id = 2,
                             Code = "develop",
                             Color = "green",
                             Comment = "Разработка",
-                            Deleted = false,
+                            IsDeleted = false,
                             Name = "Разработка"
                         },
                         new
                         {
-                            Id = 3L,
+                            Id = 3,
                             Code = "test",
                             Color = "blue",
                             Comment = "Тестирование",
-                            Deleted = false,
+                            IsDeleted = false,
                             Name = "Тестирование"
                         });
                 });
@@ -81,24 +81,24 @@ namespace Effort.DB.Layer.Migrations
                     b.Property<long>("Id")
                         .ValueGeneratedOnAdd();
 
-                    b.Property<long>("ActivityTypeId");
+                    b.Property<int>("ActivityTypeId");
 
                     b.Property<string>("Comment")
                         .HasMaxLength(250);
 
                     b.Property<DateTime>("Date");
 
-                    b.Property<bool>("Deleted");
-
-                    b.Property<long>("Duration");
+                    b.Property<int>("Duration");
 
                     b.Property<DateTime>("Inserted")
                         .ValueGeneratedOnAdd()
-                        .HasDefaultValue(new DateTime(2020, 6, 1, 16, 47, 47, 401, DateTimeKind.Local).AddTicks(5673));
+                        .HasDefaultValue(new DateTime(2020, 6, 1, 23, 16, 40, 570, DateTimeKind.Local).AddTicks(1077));
+
+                    b.Property<bool>("IsDeleted");
 
                     b.Property<DateTime>("LastUpdated")
                         .ValueGeneratedOnAdd()
-                        .HasDefaultValue(new DateTime(2020, 6, 1, 16, 47, 47, 395, DateTimeKind.Local).AddTicks(982));
+                        .HasDefaultValue(new DateTime(2020, 6, 1, 23, 16, 40, 561, DateTimeKind.Local).AddTicks(4248));
 
                     b.Property<string>("UserUniqueName")
                         .IsRequired()
@@ -114,132 +114,132 @@ namespace Effort.DB.Layer.Migrations
                         new
                         {
                             Id = -1L,
-                            ActivityTypeId = 1L,
+                            ActivityTypeId = 1,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(6171),
-                            Deleted = true,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(3280),
+                            Duration = 15,
+                            IsDeleted = true,
                             UserUniqueName = "iloer",
                             WorkItemId = 11
                         },
                         new
                         {
                             Id = -2L,
-                            ActivityTypeId = 1L,
+                            ActivityTypeId = 1,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8286),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5157),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 22
                         },
                         new
                         {
                             Id = -3L,
-                            ActivityTypeId = 1L,
+                            ActivityTypeId = 1,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8325),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5172),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 33
                         },
                         new
                         {
                             Id = -4L,
-                            ActivityTypeId = 2L,
+                            ActivityTypeId = 2,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8328),
-                            Deleted = true,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5174),
+                            Duration = 15,
+                            IsDeleted = true,
                             UserUniqueName = "iloer",
                             WorkItemId = 44
                         },
                         new
                         {
                             Id = -5L,
-                            ActivityTypeId = 2L,
+                            ActivityTypeId = 2,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8338),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5178),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 55
                         },
                         new
                         {
                             Id = -6L,
-                            ActivityTypeId = 2L,
+                            ActivityTypeId = 2,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8339),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5179),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 66
                         },
                         new
                         {
                             Id = -7L,
-                            ActivityTypeId = 2L,
+                            ActivityTypeId = 2,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8341),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5180),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 77
                         },
                         new
                         {
                             Id = -8L,
-                            ActivityTypeId = 3L,
+                            ActivityTypeId = 3,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8343),
-                            Deleted = true,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5181),
+                            Duration = 15,
+                            IsDeleted = true,
                             UserUniqueName = "iloer",
                             WorkItemId = 88
                         },
                         new
                         {
                             Id = -9L,
-                            ActivityTypeId = 3L,
+                            ActivityTypeId = 3,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8344),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5182),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 99
                         },
                         new
                         {
                             Id = -10L,
-                            ActivityTypeId = 3L,
+                            ActivityTypeId = 3,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8346),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5183),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 100
                         },
                         new
                         {
                             Id = -11L,
-                            ActivityTypeId = 3L,
+                            ActivityTypeId = 3,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8348),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5184),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 110
                         },
                         new
                         {
                             Id = -12L,
-                            ActivityTypeId = 3L,
+                            ActivityTypeId = 3,
                             Comment = "текст",
-                            Date = new DateTime(2020, 6, 1, 16, 47, 47, 403, DateTimeKind.Local).AddTicks(8349),
-                            Deleted = false,
-                            Duration = 15L,
+                            Date = new DateTime(2020, 6, 1, 23, 16, 40, 572, DateTimeKind.Local).AddTicks(5185),
+                            Duration = 15,
+                            IsDeleted = false,
                             UserUniqueName = "iloer",
                             WorkItemId = 120
                         });
