89a7ebd54cdf35d06d14bbd705625ac04f6e343d
Павел Варнавский <P_Varnavsky@step.ru>
2020-08-11 19:09:10 +0300

    fixing router

--- FrontEnd/effort-extension-vue/src/main.js
+++ FrontEnd/effort-extension-vue/src/main.js
@@ -1,27 +1,10 @@
 import Vue from 'vue'
-// import VueRouter from './router'
 import VueRouter from 'vue-router'
+import router from './router'
 import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
-import EffortExtension from './components/EffortExtension'
-import Logo from './components/Logo.vue'
 
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/effort/:workItemId',
-      name: 'workItems',
-      component: EffortExtension
-    },
-    {
-      path: '',
-      name: 'Logo',
-      component: Logo
-    }
-  ]
-})
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
