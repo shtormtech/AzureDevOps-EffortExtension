64d057c734c039aa9d52ee4dbdeb650d13ba3927
Иван Варнавский <ivan.varnavskiy@shtormtech.ru>
2020-11-01 13:57:19 +0300

    add migration controller

---
+++ BackEnd/Effort.DB.Layer/Utils/MigrationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Effort.DB.Layer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Effort.DB.Layer.Utils
+{
+    public class MigrationHelper 
+    {
+        protected string ConnectionString { get; }
+        private IEffortDbContextFactory ContextFactory { get; }
+        public MigrationHelper(string connectionString, IEffortDbContextFactory contextFactory)
+        {
+            ConnectionString = connectionString;
+            ContextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
+        }
+        public async Task MigrateAsync(CancellationToken ct = default)
+        {
+            using (var context = ContextFactory.CreateDbContext(ConnectionString))
+            {
+                await context.Database.MigrateAsync(ct);
+            }
+        }
+    }
+}
